7e5d15f8a90c3eb4dd71bc6b33d4c53674400417
leeningli <1507045477@qq.com>
2019-01-29 16:34:38 +0800

    create

---
+++ sqlpool_client.go
@@ -0,0 +1,36 @@
+package sqlpool_client
+
+import (
+	"sync"
+
+	"github.com/jinzhu/gorm"
+)
+
+type MysqlPoolClient struct {
+}
+
+var (
+	instance *MysqlPoolClient
+	once     sync.Once
+	db       *gorm.DB
+	err_db   error
+)
+
+func GetInstance() *MysqlPoolClient {
+	once.Do(func() {
+		instance = &MysqlPoolClient{}
+	})
+	return instance
+}
+
+func (m *MysqlPoolClient) InitPoolClient() (issucc bool) {
+	db, err_db = gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/nanili?charset=utf8&parseTime=True&loc=Local")
+	if err_db != nil {
+		return false
+	}
+	return true
+}
+
+func (m *MysqlPoolClient) GetMysqlPoolClient() (db_con *gorm.DB) {
+	return db
+}
